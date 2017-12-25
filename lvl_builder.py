00aae3f050989d0de56ff6b3aaa8f1f372fa6b10
Pei-Luan Tai <xination@gmail.com>
2017-12-25 12:05:47 -0500

    organize files

---
+++ lvl_builder.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python3.4
+import mytool
+import xmgrace_plot 
+import subprocess           # for calling xmgrace to plot.
+import sys
+
+##
+# internal log
+# I should have a return when the program is finished.
+
+def print_message():
+    outstr = """
+ format:
+
+ ./lvl_builder inputfile [fineParameter.txt] [output.agr]
+
+ the "fineParameter.txt" and "output.agr" are optional arguments.
+
+ "fineParameter.txt" for the fine control of presentaion details. """
+    print( outstr )
+    print("""
+    __________________________________
+    |                                |
+    |    Written by Pei-Luan Tai,    |
+    |    v1 2017, Nov 13             |
+    |________________________________|
+    """)
+
+
+
+
+def lvl_builder():
+    
+    dataFile = "demo_general_properties.txt"
+    fineControlFile = "fineParameter.txt"   
+    outputAgr = "output.agr"
+    
+    cmd = 'rm ' + outputAgr + ' 2>/dev/null'
+    subprocess.call( cmd, shell=True ) 
+
+    if( len(sys.argv) == 2  ):
+        if( sys.argv[1] == '-h' or sys.argv[1]=='--help' ):
+            print_message()
+            sys.exit( 0 )
+        else:
+            dataFile    = sys.argv[1]
+    elif(len(sys.argv) == 3  ):
+        dataFile        = sys.argv[1]
+        fineControlFile = sys.argv[2]
+    elif(  len(sys.argv) == 4  ):
+        dataFile        = sys.argv[1]
+        fineControlFile = sys.argv[2]
+        outputAgr       = sys.argv[3]
+    else:
+        print_message()
+        sys.exit( 0 )
+
+    par = mytool.Fine_parameter( fineControlFile )
+    if( par.verbose): print( " input file = ", dataFile )
+    if( par.verbose): print( " fine paraters = ",  fineControlFile )
+    if( par.verbose): print( " output agr = ", outputAgr)
+    #=================================================
+     
+    list_lvl = []
+    list_gam = []
+    list_lvl, list_gam = mytool.readin_data( dataFile )  
+    outfile = open( outputAgr ,'w')  
+    
+
+
+    # width set up 
+    #  5      10      5          
+    #^^^^^==========^^^^^
+    # total = 20
+    band_width = par.bandwidth
+    band_spacing = par.bandspacing
+    font_size = par.fontsize
+    
+    
+    #
+    # get the band structure info
+    # each element in list_lvl is a dic,
+    # and its  keys: 'bandN', 'lvlE', 'color', 'spin'
+    # 
+    # check_total_bands function will give us (1) total band number
+    # (2) smallest band index, to serve as the starting index
+    # (3) the max and min level energies, setting the y min/max range for xmgrace. 
+
+    band_totalN, band_smallest, band_largest, band_max_eng, band_min_eng \
+    = mytool.check_total_bands(list_lvl) 
+    
+    
+    
+    #
+    #  the font size calculation. The font size varies corresponding to the 
+    #  total band numbers. The smallest font size is 25 unit.
+    #
+    if band_totalN * band_width > 80:
+        font_size = int( font_size * 100./ band_totalN/band_width )
+        if font_size < par.smallestFontSize: 
+            font_size = par.smallestFontSize
+        #note: 100 is just my emperical value.
+        
+        
+    
+   
+
+    # in this step, we adjust the height for the text, such that
+    # there is no overlapped text labels for two or more closed states.
+    # 
+    # 'textY' key is for the text height, and it may be different
+    # than the level y position ( by 'lvlE' key )
+    # 'overlap' and 'olap_lvl' keys are added for processing. 
+    length = float(band_max_eng)- float(band_min_eng) # for y
+    dim = ( length, font_size )
+    pre_parse_lvl = xmgrace_plot.PRE_Separate_levels( list_lvl, dim )
+    agrYmin, agrYmax = pre_parse_lvl.Get_textY_min_max()
+
+
+
+
+    # use the Predefine_agr class object "agr_setup" to help us
+    # control the output of the agr. 
+    # this step is to set the x and y dimension
+    # band_totalN control the x dimension, text height for the y dimension.
+    Xrange =  band_totalN  * ( band_width + 2* band_spacing )
+    
+    agr_setup \
+    = xmgrace_plot.Predefine_agr (par, 0, agrYmin , Xrange, agrYmax  ) 
+  
+    outfile.write( agr_setup.preSection() )  
+    outStr= ""
+
+
+
+    #
+    # to plot the levels
+    # we add key: 'xi' and 'xf' to each dic element in list_lvl, 
+    # in Update() method.
+    band_info = ( band_smallest, band_width, band_spacing )
+    for lvl in list_lvl:
+        obj_temp = xmgrace_plot.Level( lvl, band_info )
+        obj_temp.Set_fontsize( font_size )
+        obj_temp.Set_par( par ) 
+        outStr += obj_temp.Process() 
+        obj_temp.Update( lvl) # get the position xi and xf 
+        pass
+    
+    # since some lvls could shrink, so we have to do some calcuations
+    # for the max bandL and min bandU for a given bandN
+    list_bandL, list_bandU = \
+    mytool.check_min_xi_xf_for_a_bandN( list_lvl, band_totalN, band_largest )
+    
+
+    #  each element in list_gam is a dic,
+    #  original keys: 'Ef', 'Ei', 'label', 'I', and 'color'
+    pre_parse = xmgrace_plot.PRE_Parse_gamma_level( list_lvl, \
+                                                    list_gam, \
+                                                    list_bandL, \
+                                                    list_bandU, \
+                                                    par )
+
+
+
+
+
+    list_lvl, list_gam = pre_parse.Update()
+    
+    # after pre_parse we add key for a gam: 
+    # 'xi', 'xf', 'Ei' and 'Ef' (used for final plot) 
+    # 'crossBand' maps to a boolean value.
+    # 
+    #  several keys for calculations.
+    # 'Ei_xi', 'Ei_xf', 'Ef_xi', 'Ef_xf' 
+    # 'overlap_range_xi', 'overlap_range_xf'
+    # 'overlap'  
+    #
+    
+    
+    
+    # to plot the gammas
+
+    dim = ( float(band_min_eng), float(band_max_eng),\
+            float( band_totalN*band_width) )
+    for gam in list_gam:
+        obj_temp = xmgrace_plot.Gamma( gam, list_lvl, dim )
+        obj_temp.Set_par( par ) 
+        obj_temp.Set_fontsize( font_size )
+        outStr += obj_temp.Process()
+        pass
+            
+    
+    outfile.write( outStr ) 
+    
+    
+    
+    
+    outfile.write( agr_setup.postSection() ) # just for the xmgrace file format
+    outfile.close()
+    
+    #=======================================================================
+    # generate output file
+    #
+    if( par.openXmgrace ):
+        subprocess.call( 'xmgrace ' +  outputAgr + '    &', shell=True )
+    
+    outputfile = ""
+    cmd = ""
+    if( outputAgr[-4:] == '.agr' ): outputfile = outputAgr[:-4]
+    else: outputfile = outputAgr
+    
+
+    if( par.outformat.upper() == 'PNG'): 
+        outputfile += ".png"
+        cmd = 'xmgrace '+ outputAgr + \
+          ' -hardcopy -printfile "'+outputfile+'" -hdevice PNG'
+
+    elif( par.outformat.upper() == 'PS' ):
+        outputfile += ".ps"
+        cmd = 'xmgrace '+ outputAgr + \
+          ' -hardcopy -printfile "'+outputfile+'" -hdevice PostScript'
+    
+    elif( par.outformat.upper() == 'EPS'):
+        outputfile += ".eps"
+        cmd = 'xmgrace '+ outputAgr + \
+          ' -hardcopy -printfile "'+outputfile+'" -hdevice EPS'
+    
+    subprocess.call( cmd, shell=True )
+    if(par.verbose): print( " output     = ", outputfile)    
+
+    return 1
+
+if __name__ == '__main__':
+    lvl_builder()
---
+++ mytool.py
@@ -0,0 +1,337 @@
+import sys
+#
+# to parse the input file
+#
+def readin_data( inFile ):
+    '''
+    read in data.txt, and pasre the data into
+    list_lvl and list_gam.
+    '''    
+    readIn = open( inFile ,'r')     
+    
+    lines = readIn.readlines()
+    
+    # --- ignore the commented line which starts with '#'
+    newlines = []
+    for line in lines:
+        
+        line = line.lstrip(' ')
+        if line[0] != '#':
+            newlines.append(line) 
+    lines = newlines
+    del newlines
+    
+    list_lvl = []
+    list_gam = []
+    
+     
+    for line in lines:
+        lvl = {}
+        gam = {}
+        isLvl = False
+        isGam = False
+        elements = line.split('@')
+        
+        #
+        # pre-check a line to make sure @key value pair
+        # we counts numbers of key and value to see whether they are the same.
+        checkItems = line.split() 
+        num_key = 0
+        num_value = 0
+        for item in checkItems:
+            if( item[0] == '@'): num_key += 1
+            else: num_value += 1
+        if( num_key != num_value):
+            print(" Error: key-value pair: \n", line.rstrip() )
+            print("", "^" * len(line) )
+            sys.exit(0)
+
+
+        #
+        # pre-check a line to identify lvl or gam.
+        #
+        for element in elements[1:]:        
+            mykey = element.split()[0]
+            myvalue = element.split()[1]   
+            
+            # to identify whether a level or a gamma for the current line.             
+            if (mykey == 'lvlE' or mykey == 'bandN' ):
+               isLvl = True
+               isGam = False
+            if (mykey == 'Ei' or mykey == 'Ef' ):
+               isLvl = False
+               isGam = True
+
+        if( isLvl ):
+            has_lvlE = False
+            has_bandN = False
+            for element in elements[1:]:        
+                mykey = element.split()[0]
+                myvalue = element.split()[1]
+                lvl[mykey] = myvalue
+
+                if( mykey == 'lvlE'):  has_lvlE  = True
+                if( mykey == 'bandN'): has_bandN = True
+                 
+            
+            # validate input
+            if( not has_bandN): 
+                print( " Error no bandN key")
+                print( line.rstrip() ); print("", "^" * len(line) )
+                sys.exit( 0 )
+            if( not has_lvlE ):
+                print( " Error no lvlE key")
+                print( line.rstrip() ); print("", "^" * len(line) )
+                sys.exit( 0 )
+
+            list_lvl.append( lvl )
+            
+        if( isGam ):
+            has_Ei = False
+            has_Ef = False
+            for element in elements[1:]:        
+                mykey = element.split()[0]
+                myvalue = element.split()[1]
+                gam[mykey] = myvalue
+                if( mykey == 'Ei'): has_Ei = True
+                if( mykey == 'Ef'): has_Ef = True
+
+            # validate input
+            if( not has_Ei ): 
+                print( " Error no Ei key")
+                print( line.rstrip() ); print("", "^" * len(line) )
+                sys.exit( 0 )
+            if( not has_Ef ):
+                print( " Error no Ef key")
+                print( line.rstrip() ); print("", "^" * len(line) )
+                sys.exit( 0 )
+
+            list_gam.append( gam )   
+
+    # 
+    # check gam's Ei and Ef are resigtered in lvl_list
+    # working on it
+    for gam in list_gam:
+        gamEi_found = False
+        gamEi =  gam['Ei']  # key 'Ei' map to str type value.
+        for lvl in list_lvl:
+            lvlE = lvl['lvlE'] # key 'lvlE' map to str type value.
+            if( gamEi == lvlE ): gamEi_found = True
+        if( not gamEi_found ): 
+            print( " Gamma Error: Ei =  %s is not registered yet" %gamEi )
+            sys.exit(0) 
+
+        gamEf_found = False
+        gamEf = gam['Ef']  # key 'Ei' map to str type value.
+        for lvl in list_lvl:
+            lvlE = lvl['lvlE'] # key 'lvlE' map to str type value.
+            if( gamEf == lvlE ): gamEf_found = True
+        if( not gamEf_found ): 
+            print( " Gamma Error: Ef =  %s is not registered yet" %gamEf )
+            sys.exit(0) 
+    return list_lvl, list_gam
+    pass
+
+
+
+#
+#  counting the columns ( band ) 
+#
+def check_total_bands( list_lvl ):
+    lvl_band = []
+    lvl_eng = []
+    
+    # analyze the number of the levels
+    for i in range( len( list_lvl ) ):
+        #
+        # we can have @bandN 2 
+        #          or @bandN 2_3
+        #          or @bandN 1_6
+        #          or @bandN -1
+        items = list_lvl[i]['bandN'].split('_')
+        for item in items:        
+            if item not in lvl_band:
+                lvl_band.append( int(item) )
+                                
+        pass
+     
+    bandN_total = max( lvl_band ) - min( lvl_band ) + 1
+    
+    # analyze the number of the levels
+    for i in range( len( list_lvl ) ):
+        item = list_lvl[i]['lvlE']
+        if item not in lvl_eng:
+            lvl_eng.append( float(item) )
+            # we use int (instead of float) for the energy of a level.
+        pass
+   
+    return bandN_total, min( lvl_band ), max( lvl_band ), max( lvl_eng ), min( lvl_eng )
+
+
+def check_min_xi_xf_for_a_bandN( list_lvl, bandNtotal, band_largest ):
+   
+    # the index mean the bandN
+
+    # list_bandL is to store the largest left  bound (xi) in a given bandN.
+    # list_bandU is to store the smaller right bound (xf) in a given bandN.
+    #   -----------------      
+    #   |               |
+    # bandL           bandU
+    #
+    num = bandNtotal
+    if( (band_largest+1) > bandNtotal): num = band_largest+1
+    list_bandL = [ -1   for _ in range( num ) ] 
+    list_bandU = [ 9999 for _ in range( num ) ] 
+
+    # the list index corresponds to the bandN
+    # if we have bandN from -2, -1, 0, to 4.
+    # [ 0,  1,  2,  3,  4,  -2,  -1]
+    # if we have bandN from  5, 6, 7. bandNtotal = 3, but band_largest = 7
+    # [ 0,  1,  2,  3,  4,  5,  6,  7] 
+    
+
+    for lvl in list_lvl:
+        # set short-handed notations.
+        xi = lvl['xi']
+        xf = lvl['xf']
+        bandL = lvl['bandL']
+        bandU = lvl['bandU']
+
+        if( xi >  list_bandL[ bandL ] ): list_bandL[ bandL ] = xi
+        if( xf <  list_bandU[ bandU ] ): list_bandU[ bandU ] = xf
+    
+    return list_bandL, list_bandU
+
+
+
+
+
+class Fine_parameter( ):
+    def __init__( self, setupfile ):
+
+        #default values
+        self.bandwidth = 20
+        self.bandspacing = 5
+        self.outformat = 'PS'
+        self.fontsize = 70 
+        self.smallestFontSize=25
+        self.auxlineExt = 0
+        self.arrowAdjust = 1.00   
+        self.minorShift = 0.1
+        self.levelDigit = 0
+        self.gamLabeLXOffset = 0.0
+        self.gamLabeLYOffset = 0.0
+        self.gamLabeLXLinear = 1.0
+        self.gamLabeLYLinear = 1.0
+        self.auxBandLimit = 1  
+        self.lvlshrink = 0.2
+        self.arrowLength = 0.5 
+        self.gamCrosstxtRot = 1
+        self.cornerxmin = 0.2
+        self.cornerymin = 0.2
+        self.cornerxmax = 0.13
+        self.cornerymax = 0.975
+        self.openXmgrace = 1
+<<<<<<< HEAD
+        self.outputWidth = 800  
+        self.outputHeight = 600
+        self.verbose = 1
+=======
+>>>>>>> ac7ed4770e844fa426d12e9019d9b050b5b5ada6
+
+        # start parsing
+        self.parse( setupfile )
+
+        pass
+
+    def parse( self, setupfile ):
+        lines = [] 
+        
+        with open( setupfile ) as f:
+            tmp_lines = f.readlines()
+
+        # ignore the line start with '#'
+        for line in tmp_lines:
+            if( line[0].lstrip() != '#' ): lines.append( line )
+
+        for line in lines:
+            if( line.find( 'bandwidth' ) != -1 ): 
+                self.bandwidth = int( self.get_value( line ) )
+            elif( line.find( 'bandspacing' ) != -1 ): 
+                self.bandspacing = int( self.get_value( line ) )
+            elif( line.find( 'outformat' ) != -1 ): 
+                self.outformat = str( self.get_value( line ) )
+            elif( line.find( 'fontsize' ) != -1 ): 
+                self.fontsize = int( self.get_value( line ) )
+            elif( line.find( 'smallestFontSize' ) != -1 ): 
+                self.smallestFontSize = int( self.get_value( line ) )
+            elif( line.find( 'auxlineExt' ) != -1 ): 
+                self.auxlineExt = int( self.get_value( line ) )
+            elif( line.find( 'arrowAdjust' ) != -1 ): 
+                self.arrowAdjust = float( self.get_value( line ) )
+            elif( line.find( 'minorShift' ) != -1 ): 
+                self.minorShift = float( self.get_value( line ) )
+            elif( line.find( 'levelDigit' ) != -1 ): 
+                self.levelDigit = int( self.get_value( line ) )
+            elif( line.find( 'gamLabeLXOffset' ) != -1 ): 
+                self.gamLabeLXOffset = float( self.get_value( line ) )
+            elif( line.find( 'gamLabeLYOffset' ) != -1 ): 
+                self.gamLabeLYOffset = float( self.get_value( line ) )
+            elif( line.find( 'auxBandLimit' ) != -1 ): 
+                self.auxBandLimit = int( self.get_value( line ) )
+            elif( line.find( 'lvlshrink' ) != -1 ): 
+                self.lvlshrink = float( self.get_value( line ) )
+            elif( line.find( 'gamLabeLXLinear' ) != -1 ): 
+                self.gamLabeLXLinear = float( self.get_value( line ) )
+            elif( line.find( 'gamLabeLYLinear' ) != -1 ): 
+                self.gamLabeLYLinear = float( self.get_value( line ) )
+            elif( line.find( 'arrowLength' ) != -1 ): 
+                self.arrowLength = float( self.get_value( line ) )
+            elif( line.find( 'gamCrosstxtRot' ) != -1 ): 
+                self.gamCrosstxtRot = int( self.get_value( line ) )
+            
+            elif( line.find( 'openXmgrace' ) != -1 ): 
+                self.openXmgrace = int( self.get_value( line ) )
+                
+            elif( line.find( 'cornerxmin' ) != -1 ): 
+                self.cornerxmin = str( self.get_value( line ) )
+            elif( line.find( 'cornerymin' ) != -1 ): 
+                self.cornerymin = str( self.get_value( line ) )
+            elif( line.find( 'cornerxmax' ) != -1 ): 
+                self.cornerxmax = str( self.get_value( line ) )
+            elif( line.find( 'cornerymax' ) != -1 ): 
+                self.cornerymax = str( self.get_value( line ) )
+
+<<<<<<< HEAD
+            elif( line.find( 'outputWidth' ) != -1 ): 
+                self.outputWidth = float( self.get_value( line ) )
+            elif( line.find( 'outputHeight' ) != -1 ): 
+                self.outputHeight = float( self.get_value( line ) )
+
+            elif( line.find( 'verbose' ) != -1 ): 
+                self.verbose = int( self.get_value( line ) )
+=======
+>>>>>>> ac7ed4770e844fa426d12e9019d9b050b5b5ada6
+         
+            pass    
+        
+        pass
+
+    def get_value( self, line ):
+        items = line.split('=')
+        values = items[1].split()
+        return values[0]
+         
+#
+#   work: I may need to clean this
+#
+if __name__ == '__main__':
+
+    par = Fine_parameter( "fineParameter.txt")
+
+    # list_lvl = []
+    # list_gam = []
+    # list_lvl, list_gam = readin_data('testData.txt')
+
+
+
